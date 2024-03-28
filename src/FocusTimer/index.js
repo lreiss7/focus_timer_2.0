26b08747ff52a86c5423bf3f0a71d24ec44a1511
Luan <luuanreis@gmail.com>
2024-03-28 06:26:56 -0300

    creating initial state of the application

--- src/FocusTimer/index.js
+++ src/FocusTimer/index.js
@@ -1,3 +1,7 @@
+import state from './state.js'
 export function start(minutes, seconds) {
-  console.log(minutes, seconds)
+  state.minutes = minutes
+  state.seconds = seconds
+
+  console.log(state)
 }
