df61f1fbb408e964e853b0915c8bef26a4ad9f7b
Luan <luuanreis@gmail.com>
2024-03-31 09:29:13 -0300

    recording button events

---
+++ src/FocusTimer/actions.js
@@ -0,0 +1,21 @@
+// import state from './state.js'
+
+export function toggleRunning() {
+  console.log("toggleRunning")
+}
+
+export function toggleReset() {
+  console.log("toggleReset")
+}
+
+export function toggleMusic() {
+  console.log("toggleMusic")
+}
+
+export function toggleAdd() {
+  console.log("toggleAdd")
+}
+
+export function toggleRemove() {
+  console.log("toggleRemove")
+}
---
+++ src/FocusTimer/events.js
@@ -0,0 +1,22 @@
+import { controls, sounds } from './elements.js'
+import * as actions from './actions.js'
+
+export function registerControls () {
+  controls.addEventListener('click', (event) => {
+    const action = event.target.dataset.action
+    if(typeof actions[action] != "function") {
+      return
+    }
+
+    actions[action]()
+  })
+  
+  sounds.addEventListener('click', (event) => {
+    const action = event.target.dataset.action
+    if(typeof actions[action] != "function") {
+      return
+    }
+
+    actions[action]()
+  })
+}
