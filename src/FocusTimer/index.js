fbb2a722c8d8867bc2077474811b170118d40ede
Luan <luuanreis@gmail.com>
2024-03-31 09:25:21 -0300

    index.js modified

--- src/FocusTimer/index.js
+++ src/FocusTimer/index.js
@@ -1,7 +1,9 @@
 import state from './state.js'
+import * as events from './events.js'
+
 export function start(minutes, seconds) {
   state.minutes = minutes
   state.seconds = seconds
 
-  console.log(state)
+  events.registerControls()
 }
