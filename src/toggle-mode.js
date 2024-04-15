d630dc6e15cb9c2314f53b446b1d3c1f2508092c
Luan <luuanreis@gmail.com>
2024-04-15 06:12:12 -0300

    updating dark mode to automatic

--- src/toggle-mode.js
+++ src/toggle-mode.js
@@ -1,13 +1,37 @@
 let lightMode = true
 const buttonToggle = document.getElementById('toggle-mode')
 
-buttonToggle.addEventListener('click', (event) => {
-  document.documentElement.classList.toggle('dark')
-
-  const mode = lightMode ? 'dark' : 'light'
-
-  event.currentTarget
-    .querySelector('span').textContent = `${mode} mode ativado!`
-
+function toggleMode() {
   lightMode = !lightMode
-})
+  document.documentElement.classList.toggle('dark', !lightMode)
+
+  const modeText = lightMode ? 'Dark mode ativado!' :  'Light mode ativado'
+  buttonToggle.querySelector('span').textContent = modeText
+}
+
+buttonToggle.addEventListener('click', toggleMode)
+
+const savedMode = localStorage.getItem('lightMode')
+if (savedMode !== null) {
+  lightMode = savedMode === 'true'
+  toggleMode()
+}
+
+function saveMode() {
+  localStorage.setItem('lightMode', lightMode)
+}
+
+window.addEventListener('beforeunload', saveMode)
+
+
+
+// buttonToggle.addEventListener('click', (event) => {
+//   document.documentElement.classList.toggle('dark')
+
+//   const mode = lightMode ? 'dark' : 'light'
+
+//   event.currentTarget
+//     .querySelector('span').textContent = `${mode} mode ativado!`
+
+//   lightMode = !lightMode
+// })
